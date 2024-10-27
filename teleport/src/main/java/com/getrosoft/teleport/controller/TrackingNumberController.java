b6a842ef392b264560e83f6ccf549e4ff0362cce
BOGA\bogas <bogacnutctk@gmail.com>
2024-10-27 23:43:50 +0530

    Teleport: Do modify

--- teleport/src/main/java/com/getrosoft/teleport/controller/TrackingNumberController.java
+++ teleport/src/main/java/com/getrosoft/teleport/controller/TrackingNumberController.java
@@ -1,5 +1,6 @@
 package com.getrosoft.teleport.controller;
 
+import com.getrosoft.teleport.service.TrackingNumberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
